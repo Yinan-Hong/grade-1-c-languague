2f43e50abdbe20b916ab9095c9d74f565be85770
Yinan-Hong <64832229+Yinan-Hong@users.noreply.github.com>
2020-05-05 17:01:40 +0800

    Update lab5ph.c

--- lab5ph.c
+++ lab5ph.c
@@ -18,3 +18,32 @@ int main(int argc, char *argv[]) {
 }
 	return 0;
 }
+/*设有函数y = f(x)定义如下(图...)。输入x的值，计算y值并输出(保留2位小数）。
+
+提示：求绝对值函数 ：double fabs(double x)。
+
+           正弦函数：double sin(double x)。
+
+           开根号函数：double sqrt(double x)。
+
+                头文件：math.h
+
+
+
+输入
+测试次数
+
+每组测试数据一行，为x的值
+
+输出
+对每个x，输出计算得到的y值
+
+样例输入
+2
+-10.0
+5.0
+
+样例输出
+10.00
+20.00
+*/
