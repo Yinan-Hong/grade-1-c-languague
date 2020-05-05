b43cb32914507c6b869172212e35c74c9cd1fdbf
Yinan-Hong <64832229+Yinan-Hong@users.noreply.github.com>
2020-05-05 16:40:50 +0800

    Update Lab1 pF.c

--- Lab1 pF.c
+++ Lab1 pF.c
@@ -20,3 +20,15 @@ int main(int argc, char *argv[]) {
 	is_prime(n)	;
 	return 0;
 }
+/*
+题目描述
+写一个判断素数的函数，在主函数输入一个整数，输出是否是素数的消息。
+输入
+一个数
+输出
+如果是素数输出prime 如果不是输出not prime
+样例输入
+97
+样例输出
+prime
+*/
