d8acf4e0e43580bca4f544517decb8e33068d0c0
Yinan-Hong <64832229+Yinan-Hong@users.noreply.github.com>
2020-05-05 16:57:49 +0800

    Update lab5pc.c

--- lab5pc.c
+++ lab5pc.c
@@ -12,3 +12,20 @@ int main(int argc, char *argv[]) {
 	printf("%d",y);
 	return 0;
 }
+/*
+题目描述
+输入一个字符，判定该字符是否是字母。
+
+(提示：可使用ctype.h中的函数）
+
+输入
+字符
+
+输出
+若该字符是字母，输出ALPHA; 否则，输出NO
+
+样例输入
+Y
+样例输出
+ALPHA
+*/
