337da393307534dd631d566f6a1b81f9b4792895
Yinan-Hong <64832229+Yinan-Hong@users.noreply.github.com>
2020-05-05 16:42:45 +0800

    Update Lab2 pB.c

--- Lab2 pB.c
+++ Lab2 pB.c
@@ -18,3 +18,18 @@ int main(int argc, char *argv[]) {
 	else printf("%d",c);
 	return 0;
 }
+/*
+题目描述
+编写一个程序，输入a、b、c三个值，输出其中最大值。
+
+输入
+一行数组，分别为a b c
+
+输出
+a b c其中最大的数
+
+样例输入
+10 20 30
+样例输出
+30
+*/
